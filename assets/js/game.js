e36bbe2111e003076f98ca6fb693d22a9f762520
Gustavo Gibo <gustavogibo@Gustavos-MacBook-Pro.local>
2018-04-24 10:41:10 -0700

    Declaring all the possible variables so far

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,9 +10,22 @@ Variables needed for the game:
 
 */
 
-// Declaring the var randomLetter (responsible for putting )
+// Declaring the var randomLetter (responsible for storaging the letter to be guesses)
 var randomLetter = "";
+
+/*
+    Using three native functions to generate my random letter
+    fromCharCode = Returns a string created from the specified sequence of UTF-16 code units.
+    math.floor = Returns the largest integer less than or equal to a given number.
+    math.random = Returns a floating-point, pseudo-random number in the range from 0 inclusive up to but not including 1
+*/
 randomLetter = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 
-console.log(randomLetter);
+// Declaring Wins, Losses, Guesses and Actual Guesses
+var wins = 0;
+var losses = 0;
+var guesses = 0;
+var actualGuesses = 0;
+
+// console.log(randomLetter);
 
