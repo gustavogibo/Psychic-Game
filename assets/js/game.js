27179176b867660625312fd7915a4da8f8d5e307
Gustavo Gibo <gustavogibo@Gustavos-MacBook-Pro.local>
2018-04-24 23:19:22 -0700

    creating the logic about what happens if the user have no more guesses left

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,17 +9,13 @@ Variables needed for the game:
 - Letter to be guessed = randomLetter
 
 */
-
-// Declaring the var randomLetter (responsible for storaging the letter to be guesses)
-var randomLetter = "";
-
 /*
     Using three native functions to generate my random letter
     fromCharCode = Returns a string created from the specified sequence of UTF-16 code units.
     math.floor = Returns the largest integer less than or equal to a given number.
     math.random = Returns a floating-point, pseudo-random number in the range from 0 inclusive up to but not including 1
 */
-randomLetter = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+var randomLetter = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 
 // Declaring Wins, Losses, Guesses and Actual Guesses
 var wins = 0;
@@ -30,8 +26,39 @@ var guessList = [];
 
 var divWin = document.getElementById("user-choice");
 var divLoss = document.getElementById("user-losses");
+var divGuess = document.getElementById("guess-number");
 var divGuessLeft = document.getElementById("guess-left");
 var divGuessSoFar = document.getElementById("guess-so-far");
+
+document.onkeyup = function(event) {
+
+    var userGuess = event.key;
+        userGuess = userGuess.toLowerCase();
+
+    if(userGuess != randomLetter) {
+
+        guessesLeft--;
+        
+        if (guessesLeft == 0) {
+
+            randomLetter = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+            losses++;
+            guesses = 0;
+            guessesLeft = 9;
+            guessList = [];
+
+            divLoss.textContent = losses;
+            divGuess.textContent = guesses;
+            divGuessLeft.textContent = guessesLeft;
+            divGuessSoFar.textContent = "---";
+            
+        } else {
+            
+        }
+        guessList.push(userGuess);
+    }
+
+};
 
 // console.log(randomLetter);
 
